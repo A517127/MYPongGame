9d2999725083ff78cf5d4e0c5a629f76ca1b239e
Siriwimon Mawong <67771027+A517127@users.noreply.github.com>
2020-08-15 20:22:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myponggame/Bat.java
@@ -0,0 +1,57 @@
+package com.example.myponggame;
+
+import android.graphics.RectF;
+
+public class Bat {
+    //attribute.......................
+   private RectF mRect;
+   private float mLength;
+   private float mXCoord;
+   private float mBatSpeed;
+   private int mScreenX;
+   final int STOPPED =0;
+   final int LEFT = 1;
+   final int RIGHT = 2;
+   private  int mBatMoving = STOPPED;
+
+   //constructor.........................
+   Bat(int sx, int sy){
+       mScreenX = sx;
+       mLength = mScreenX / 8;
+       float height = sy / 40;
+       mXCoord = mScreenX / 2;
+       float mYCoord = sy - height;
+       mRect = new RectF(mXCoord, mYCoord,
+               mXCoord + mLength,
+               mYCoord + height);
+       mBatSpeed = mScreenX;
+   }
+
+   //method.....................................
+   void setMovementState(int state){
+       mBatMoving = state;
+
+   }
+
+    RectF getRect(){ return mRect; }
+
+    void update(long fps){
+        if(mBatMoving == LEFT){
+            mXCoord = mXCoord - mBatSpeed / fps;
+        }
+        if(mBatMoving == RIGHT){
+            mXCoord = mXCoord + mBatSpeed / fps;
+        }
+        if(mXCoord < 0){
+            mXCoord = 0;
+        }
+        else if(mXCoord + mLength > mScreenX){
+            mXCoord = mScreenX - mLength;
+        }
+        mRect.left = mXCoord;
+        mRect.right = mXCoord + mLength;
+    }
+
+
+
+}
